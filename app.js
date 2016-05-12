4104874920a1fd570a351b83924aca39a32e8638
Adrian Barnes <adrian@apexwebdev.com>
2016-05-12 15:04:04 -0700

    Got handlebars working :)

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ var path = require('path'); //Path is used to concatenate paths for multiplatfor
 
 
 var app = express();
-app.engine('handlebars', exHandlebars({ defaultLayout: 'main'}));
+app.engine('handlebars', exHandlebars({ defaultLayout: 'default'}));
 // app.engine('handlebars',handlebars.engine);
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','handlebars');
 app.use(express.static('bower_components'));
 
 app.get('/',function(req,res){
-    res.render("index");
+    res.render("index",{name:"adrian"});
     console.log("requested",req.url);
 });
 
