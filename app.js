0bc9b99aea3a0c0f0108bfc2902e6cf658e1f110
Adrian Barnes <adrian@apexwebdev.com>
2016-05-12 14:43:18 -0700

    working in handlebars

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var http = require('http'); //Built-in Node Functionality
 
-var express = require('express'); //Express for middleware
+var express = require('express');
+var exHandlebars = require('express-handlebars'); //Express for middleware
 // var handlebars = require('express-handlebars').create({defaultLayout:"index"});
 var path = require('path'); //Path is used to concatenate paths for multiplatform stability
     //gulp = require('gulp');
 
 
 var app = express();
+app.engine('handlebars', exHandlebars({ defaultLayout: 'main'}));
 // app.engine('handlebars',handlebars.engine);
 app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs');
+app.set('view engine','handlebars');
 app.use(express.static('bower_components'));
 
 app.get('/',function(req,res){
